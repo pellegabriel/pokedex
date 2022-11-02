77818c3c53890a94ee4461ba0645c6adb92600d7
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2022-11-02 15:21:46 -0300

    css backgrounds apply

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Main from './Components/Main';
 import './Components/styles.css'
+import Header from './Components/Header';
 function App() {
   return (
     <>
+      <div className='pokeheader'>
+      <Header/>
+      </div>
       <Main/>
     </>
   );
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+        <h1>Pokemon</h1>
+        <img className='imgpoke' src='./poke.png' alt=''/>
+        </>
+    )
+}
+
+export default Header
